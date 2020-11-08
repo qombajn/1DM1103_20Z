9427a55cc45f3332096524570c590501fb5ab17a
qombajn <kacper01deleska@gmail.com>
2020-11-08 05:26:58 +0100

    Ostateczna wersja

--- sem5/mymathlab.c
+++ sem5/mymathlab.c
@@ -24,246 +24,208 @@ void wczytaj(FILE * fin, struct macierz *m)
         }
     }
 }
-void wypisz(struct macierz m) 
+void wypisz(struct macierz *m) 
 {
-    int i, j;
-    printf("[ ");
-    for (i = 0; i < m.w; i++) {
-        for (j=0; j < m.k; j++) {
-            printf("%5.1f ", m.tab[i][j]);
-        }
-        if (i < (m.w-1))
-            printf("\n  ");
-    }
-    printf("]\n");
-}
-void zapisz(char nazwa [],struct macierz m)
 {
-FILE *f;
-f=fopen(nazwa,"w+");
-fprintf(f,"To jest macierz wynik\n[");
-for(int i = 0; i < m.w; i++)
+printf("To jest wynik\n[\t");
+for(int i = 0; i < m->w; i++)
     {
-        for(int j = 0; j <m.k; j++)
+        for(int j = 0; j < m->k; j++)
         {
-            fprintf(f," %.1f ",m.tab[i][j]);
-            if(j+1 == m.k)
-            fprintf(f,"]\n");
+            printf("%.2f\t", m->tab[i][j]); //żeby było ładnie przejrzyście i czytelnie to dałem dwie po przecinku 
+            if(j+1 == m->k)
+            printf("]\n");
         } 
-    if(i+1 != m.w)   
-    fprintf(f,"[");
+    if(i+1 != m->w)   
+    printf("[\t");
     }
-fclose(f);
 }
-struct macierz suma(struct macierz A, struct macierz B)
+}
+void zapisz(FILE * fin,struct macierz *m)
 {
-if((A.w!=B.w)&&(A.k!=B.k))
+fprintf(fin,"%d\n%d\n",m->w,m->k);
+for(int i = 0; i < m->w; i++)
+    {
+        for(int j = 0; j <m->k; j++)
+        {
+            fprintf(fin,"%.2f\t",m->tab[i][j]);
+            if(j+1 == m->k)
+            fprintf(fin,"\n");
+        }  
+    }
+fclose(fin);
+}
+void suma(struct macierz *A, struct macierz *B, struct macierz *W)
+{
+if((A->w!=B->w)&&(A->k!=B->k))
 {
 printf("Tych macierzy nie da się dodać.\n");
-exit(-1);
 }
 else
 {
-struct macierz tab;
-for(int i=0; i<A.k; i++)
-    for(int j=0;i<B.w;j++)
-    {
-    tab.tab[i][j] = A.tab[i][j] + B.tab[i][j];
-    }
-return tab;
+W->w = A->w;
+W->k = A->k;
+for(int i=0; i < A->k; i++)
+    for(int j=0; j < B->w; j++)
+    W->tab[i][j] = A->tab[i][j] + B->tab[i][j];
 }
 }
-struct macierz substract(struct macierz A, struct macierz B)
+void subtract(struct macierz *A, struct macierz *B, struct macierz *W)
 {
-if((A.w!=B.w)&&(A.k!=B.k))
+if((A->w!=B->w)&&(A->k!=B->k))
 {
 printf("Tych macierzy nie da się odjąć.\n");
-exit(-1);
 }
 else
 {
-struct macierz tab;
-for(int j=0;j<B.w;j++)
-    for(int i=0; i<A.k; i++)
-    {
-    tab.tab[i][j] = A.tab[i][j] - B.tab[i][j];
-    }
-return tab;
+W->w = A->w;
+W->k = A->k;
+for(int i=0; i < A->k; i++)
+    for(int j=0; j < B->w; j++)
+    W->tab[i][j] = A->tab[i][j] - B->tab[i][j];
 }
 }
-struct macierz prod(struct macierz A, struct macierz B)
+void prod(struct macierz *A, struct macierz *B, struct macierz *W)
 {
-if(A.k!=B.w)
+if(A->k!=B->w)
 {
-printf("Tych macierzy nie pomnożysz\n");
-exit(-1);
+printf("Tych macierzy nie pomnoży się niestety\n");
 }
 else
 {
-    struct macierz iloczyn;
-    for(int i = 0; i < A.w; i++)
+W->w = A->w;
+W->k = B->k;
+    for(int i = 0; i < A->w; i++)
     {
-        for(int j = 0; j < B.k; j++)
+        for(int j = 0; j < B->k; j++)
         {   
-            for(int k = 0; k<A.k; k++)
+            for(int k = 0; k < A->k; k++)
             {
-            float c = A.tab[i][k]*B.tab[k][j];
-            c += iloczyn.tab[i][j];
-
+            float c = A->tab[i][k]*B->tab[k][j];
+            W->tab[i][j] += c;
+            c = 0;
             }
         } 
     }
-return iloczyn;
 }
 }
-struct macierz multiply(struct macierz A, float skalar)
+void multiply(struct macierz *A, float skalar, struct macierz *W)
 {
-struct macierz tab;
-for(int j=0;j<A.w;j++)
-    for(int i=0; i<A.k; i++)
-    {
-    tab.tab[i][j] = A.tab[i][j] * skalar;
-    }
-return tab;
+W->w = A->w;
+W->k = A->k;
+for(int j=0;j<A->w;j++)
+    for(int i=0; i<A->k; i++)
+    W->tab[i][j] = A->tab[i][j] * skalar;
 }
-float norm(struct macierz m) {
+float norm(struct macierz *m) 
+{
     float s = 0;
     int i,j;
 
-    for (i = 0; i < m.w; i++) {
-        for (j=0; j < m.k; j++) {
-            s += m.tab[i][j] * m.tab[i][j];
+    for (i = 0; i < m->w; i++) {
+        for (j=0; j < m->k; j++) {
+            s += m->tab[i][j] * m->tab[i][j];
         }
     }
     return s;
 }
 
 
-
 int main(int argc, char *argv[])
 {
-struct macierz A;
-struct macierz B;
+struct macierz A = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+struct macierz B = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+struct macierz W = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 FILE * fileA;
 FILE * fileB;
 FILE * fileW;
 
+
+
 if(strcmp(argv[1], "sum")==0)
 {
-    if(strcmp(argv[4],"W.txt")==0) // do pliku
-    {
-    fileA = fopen(argv[2],"r");
-    wczytaj(fileA, &A);
-    fclose(fileA);
-    fileB = fopen(argv[3],"r");
-    wczytaj(fileB, &B);
-    fclose(fileB);
-    //fileW = fopen(argv[4],"w+");
-    zapisz(argv[4],suma(A,B));
-    //fclose(fileW);
-    }
-        else // na tablice
-        {
-        fileA = fopen(argv[2],"r");
-        wczytaj(fileA, &A);
-        fclose(fileA);
-        fileB = fopen(argv[3],"r");
-        wczytaj(fileB, &B);
-        fclose(fileB);
-        wypisz(suma(A,B));
-        }
+{
+fileA = fopen(argv[2],"r"); //odtąd
+wczytaj(fileA,&A);
+fclose(fileA);
+fileB = fopen(argv[3],"r");
+wczytaj(fileB,&B);
+fclose(fileB);              //dotąd chciałem wkleić przed pętle ale się nie dało bo potem argv[3] był potrzebny do skalaru
+suma(&A,&B,&W);
+wypisz(&W);                 
 }
-else if(strcmp(argv[1], "subtract")==0)
+    if(strcmp(argv[4],"do_b.txt")==0) // do pliku
     {
-    if(strcmp(argv[4],"W.txt")==0) // do pliku
-    {
-    fileA = fopen(argv[2],"r");
-    wczytaj(fileA, &A);
-    fclose(fileA);
-    fileB = fopen(argv[3],"r");
-    wczytaj(fileB, &B);
-    fclose(fileB);
     fileW = fopen(argv[4],"w+");
-    zapisz(argv[4],substract(A,B));
+    zapisz(fileW,&W);
     fclose(fileW);
     }
-        else // na tablice
-        {
-        fileA = fopen(argv[2],"r");
-        wczytaj(fileA, &A);
-        fclose(fileA);
-        fileB = fopen(argv[3],"r");    
-        wczytaj(fileB, &B);
-        fclose(fileB);
-        wypisz(substract(A,B));
-        }
+}
+else if(strcmp(argv[1], "subtract")==0)
+{
+fileA = fopen(argv[2],"r");
+wczytaj(fileA,&A);
+fclose(fileA);
+fileB = fopen(argv[3],"r");
+wczytaj(fileB,&B);
+fclose(fileB);
+subtract(&A,&B,&W);
+wypisz(&W);
+    if(strcmp(argv[4],"do_b.txt")==0) // do pliku
+    {
+    fileW = fopen(argv[4],"w+");
+    zapisz(fileW,&W);
+    fclose(fileW);
+    }
 }
 else if(strcmp(argv[1], "prod")==0)
 {
-    if(strcmp(argv[4],"W.txt")==0) // do pliku
+fileA = fopen(argv[2],"r");
+wczytaj(fileA,&A);
+fclose(fileA);
+fileB = fopen(argv[3],"r");
+wczytaj(fileB,&B);
+fclose(fileB);
+prod(&A,&B,&W);
+wypisz(&W);
+    if(strcmp(argv[4],"do_b.txt")==0) // do pliku
     {
-    fileA = fopen(argv[2],"r");
-    wczytaj(fileA, &A);
-    fclose(fileA);
-    fileB = fopen(argv[3],"r");
-    wczytaj(fileB, &B);
-    fclose(fileB);
     fileW = fopen(argv[4],"w+");
-    zapisz(argv[4],prod(A,B));
-    fclose(fileW);
-
+    zapisz(fileW,&W);
+    fclose(fileW); 
     }
-        else // na tablice
-        {
-        fileA = fopen(argv[2],"r");
-        wczytaj(fileA, &A);
-        fclose(fileA);
-        fileB = fopen(argv[3],"r");
-        wczytaj(fileB, &B);
-        fclose(fileB);
-        wypisz(prod(A,B));
-        }
 }
 else if(strcmp(argv[1], "multiply")==0)
 {
-    if(strcmp(argv[4],"W.txt")==0) // do pliku
+fileA = fopen(argv[2],"r");
+wczytaj(fileA,&A);
+fclose(fileA);
+float skalar = atof(argv[3]);
+multiply(&A,skalar,&W);
+wypisz(&W);
+    if(strcmp(argv[4],"do_b.txt")==0) // do pliku
     {
-    float skalar = atof(argv[3]);
-    fileA = fopen(argv[2],"r");
-    wczytaj(fileA, &A);
-    fclose(fileA);
     fileW = fopen(argv[4],"w+");
-    zapisz(argv[4],multiply(A,skalar));
-    fclose(fileW);
+    zapisz(fileW,&W);
+    fclose(fileW);  
     }
-        else // na tablice
-        {
-        fileA = fopen(argv[2],"r");
-        float skalar = atof(argv[3]);
-        wczytaj(fileA, &A);
-        fclose(fileA);
-        wypisz(multiply(A,skalar));
-        }
 }
 else if(strcmp(argv[1], "norm")==0)
 {
-    if(strcmp(argv[3],"W.txt")==0) // do pliku
+{
+fileA = fopen(argv[2],"r");
+wczytaj(fileA,&A);
+fclose(fileA);
+float s = sqrt(norm(&A));
+printf("Norma zadanej macierzy wynosi: %.3f\n",s);
+}
+    if(strcmp(argv[3],"do_b.txt")==0) // do pliku
     {
-    fileA = fopen(argv[2],"r");
-    wczytaj(fileA, &A);
-    fclose(fileA);
+    float s = sqrt(norm(&A));
     fileW = fopen(argv[3],"w+");
-    fprintf(fileW,"Norma macierzy wynosi:%f",norm(A));
+    fprintf(fileW,"Norma zadanej macierzy wynosi: %.3f\n",s);
     fclose(fileW);
-
     }
-        else // na tablice
-        {
-        fileA = fopen(argv[2],"r");
-        wczytaj(fileA, &A);
-        fclose(fileA);
-        printf("Norma macierzy wynosi:%f",norm(A));
-        }
 }
 else
 {
